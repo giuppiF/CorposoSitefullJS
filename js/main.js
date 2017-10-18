6c86d053043a750ba3fbcc96bd0cffba78e8e075
Giuppi <g.funicello@gmail.com>
2017-10-18 11:36:49 +0200

    Giuppi version

--- js/main.js
+++ js/main.js
@@ -1,20 +1,30 @@
+// noinspection Annotator
+// noinspection Annotator
 $(document).ready(function(){
   
   // variables
-  var $header_top = $('.header-top');
-  var $nav = $('nav');
+  // noinspection Annotator
+    var $header_top = $('.header-top');
+  // noinspection Annotator
+    var $nav = $('nav');
 
 
 
   // toggle menu 
-  $header_top.find('a').on('click', function() {
-    $(this).parent().toggleClass('open-menu');
+  // noinspection Annotator
+    // noinspection Annotator
+    $header_top.find('a').on('click', function() {
+    // noinspection Annotator
+        // noinspection Annotator
+        $(this).parent().toggleClass('open-menu');
   });
 
 
 
   // fullpage customization
-  $('#fullpage').fullpage({
+  // noinspection Annotator
+    // noinspection Annotator
+    $('#fullpage').fullpage({
     sectionsColor: ['#B8AE9C', '#348899', '#F2AE72', '#5C832F', '#B8B89F'],
     sectionSelector: '.vertical-scrolling',
     slideSelector: '.horizontal-scrolling',
@@ -26,28 +36,50 @@ $(document).ready(function(){
     menu: '#menu',
 
     afterLoad: function(anchorLink, index) {
-      $header_top.css('background', 'rgba(0, 47, 77, .3)');
-      $nav.css('background', 'rgba(0, 47, 77, .25)');
+      // noinspection Annotator
+        $header_top.css('background', 'rgba(0, 47, 77, .3)');
+      // noinspection Annotator
+        $nav.css('background', 'rgba(0, 47, 77, .25)');
       if (index == 5) {
+          // noinspection Annotator
+          // noinspection Annotator
           $('#fp-nav').hide();
         }
     },
 
     onLeave: function(index, nextIndex, direction) {
       if(index == 5) {
-        $('#fp-nav').show();
+        // noinspection Annotator
+          $('#fp-nav').show();
       }
     },
 
     afterSlideLoad: function( anchorLink, index, slideAnchor, slideIndex) {
       if(anchorLink == 'fifthSection' && slideIndex == 1) {
-        $.fn.fullpage.setAllowScrolling(false, 'up');
-        $header_top.css('background', 'transparent');
-        $nav.css('background', 'transparent');
-        $(this).css('background', '#374140');
-        $(this).find('h2').css('color', 'white');
-        $(this).find('h3').css('color', 'white');
-        $(this).find('p').css(
+        // noinspection Annotator
+          // noinspection Annotator
+          // noinspection Annotator
+          // noinspection Annotator
+          $.fn.fullpage.setAllowScrolling(false, 'up');
+        // noinspection Annotator
+          $header_top.css('background', 'transparent');
+        // noinspection Annotator
+          $nav.css('background', 'transparent');
+        // noinspection Annotator
+          // noinspection Annotator
+          $(this).css('background', '#374140');
+        // noinspection Annotator
+          // noinspection Annotator
+          // noinspection Annotator
+          $(this).find('h2').css('color', 'white');
+        // noinspection Annotator
+          // noinspection Annotator
+          // noinspection Annotator
+          $(this).find('h3').css('color', 'white');
+        // noinspection Annotator
+          // noinspection Annotator
+          // noinspection Annotator
+          $(this).find('p').css(
           {
             'color': '#DC3522',
             'opacity': 1,
@@ -59,9 +91,15 @@ $(document).ready(function(){
 
     onSlideLeave: function( anchorLink, index, slideIndex, direction) {
       if(anchorLink == 'fifthSection' && slideIndex == 1) {
-        $.fn.fullpage.setAllowScrolling(true, 'up');
-        $header_top.css('background', 'rgba(0, 47, 77, .3)');
-        $nav.css('background', 'rgba(0, 47, 77, .25)');
+        // noinspection Annotator
+          // noinspection Annotator
+          // noinspection Annotator
+          // noinspection Annotator
+          $.fn.fullpage.setAllowScrolling(true, 'up');
+        // noinspection Annotator
+          $header_top.css('background', 'rgba(0, 47, 77, .3)');
+        // noinspection Annotator
+          $nav.css('background', 'rgba(0, 47, 77, .25)');
       }
     } 
   }); 
